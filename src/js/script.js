1c8faa28dece33cc19f81f473d1ced00491765ac
Evgen <Evgeniy.shmat@gmail.com>
2021-01-15 19:47:16 +0300

    fix error with way to js file

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         speed: 300,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/chevron_left.png"></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="../icon/chevron_right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/chevron_left.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icon/chevron_right.png"></button>',
         responsive: [
             {
               breakpoint: 992,
