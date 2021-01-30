37f9fa146f9362a47d5c40c9c10fdcce2fa1cbd6
Evgen <Evgeniy.shmat@gmail.com>
2021-01-31 00:23:33 +0300

    make validation of forms, add sending of order

--- src/js/script.js
+++ src/js/script.js
@@ -14,58 +14,124 @@
 //       }]
 //     });
 //   });
+$(document).ready(function () {
+	const slider = tns({
+		container: '.carousel__inner',
+		items: 1,
+		slideBy: 'page',
+		autoplay: false,
+		controls: false,
+		nav: false,
+		responsive: {
+			576: {
+				nav: true,
 
-const slider = tns({
-  container: '.carousel__inner',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  controls: false,
-  nav: false,
-  responsive: {
-	576: {
-	  nav: true,
+			},
+			768: {
+				nav: false,
+			},
+			992: {
 
-	},
-	768: {
-	  nav: false,
-	},
-	992: {
-	  
-	}
-	}
+			}
+		}
+	});
+
+	document.querySelector('.prev').addEventListener('click', function () {
+		slider.goTo('prev');
+	});
+
+	document.querySelector('.next').addEventListener('click', function () {
+		slider.goTo('next');
+	});
 });
 
-document.querySelector('.prev').addEventListener('click', function () {
-  slider.goTo('prev');
-});
+$(document).ready(function ($) {
+	$(function () {
+		$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)',
+			function () {
+				$(this)
+					.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+					.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+			});
+	});
 
-document.querySelector('.next').addEventListener('click', function () {
-  slider.goTo('next');
-});
-
-
-(function($) {
-  	$(function() {
-		$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-			$(this)
-				.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-				.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-		});
-    });
-
-    function toggleSlide(item) {
-		$(item).each(function(i) {
-			$(this).on('click', function(e) {
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
 				e.preventDefault();
 				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
 				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-		
-			});
-		}); 
-    };
 
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
+			});
+		});
+	}
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+
+	// Modal
+
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
+
+	function valideForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 3,
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				},
+			},
+			messages: {
+				name: {
+					required: "Имя напиши!",
+					minlength: jQuery.validator.format("введи хотяб {0} букавы!")
+				},
+				phone: "Мобилу оставить надо!",
+				email: {
+					required: "Мыло черкани давай!",
+					email: "Шо забыл как емеил пишется?!"
+				}
+			}
+		});
+	}
+
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-999");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize ()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	}); 
+
 
 })(jQuery);
